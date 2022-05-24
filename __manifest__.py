f2dc936dd8fb9494aadfc1460fcb07d2367c3cc8
Pau Lafoz <pau@lacodornella.cat>
2022-05-24 23:10:39 +0200

    Basic module structure created

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Point of sale - Prices with VAT",
+    "summary": """This module shows the prices with VAT on the POS product's list""",
+    "author": "Pau Lafoz (@pau-lafoz) - Supercoop Manresa",
+    "website": "",
+    "category": "Point Of Sale",
+    "version": "12.0.0.0",
+    "depends": ["beesdoo_base", "beesdoo_product"],
+    "qweb": ["static/src/xml/templates.xml"],
+    "installable": True,
+    "license": "AGPL-3",
+}
